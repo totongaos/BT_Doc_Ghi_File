7e4d8add787e5eef4bc25e538d991440c30e51a9
totongaos <totongaos5@gmail.com>
2023-08-22 23:28:12 +0700

    First Push

--- learn92.py
+++ learn92.py
@@ -1,3 +1,5 @@
 # File 2to10.inp là file chứa các số nhị phân (mỗi dòng 1 số), hãy chuyển các số nhị phân đó sang số thập phân
 # rồi lưu lại vào file  2to10.out (mỗi dòng 1 số)
 print("Hello")
+print("Hello")
+
