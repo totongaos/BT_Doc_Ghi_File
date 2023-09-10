1b24f9e7a53ebe3c85796bdc142ded50a9ebea01
totongaos <totongaos5@gmail.com>
2023-09-10 10:04:45 +0700

    10.09.23

--- learn92.py
+++ learn92.py
@@ -1,6 +1,20 @@
 # File 2to10.inp là file chứa các số nhị phân (mỗi dòng 1 số), hãy chuyển các số nhị phân đó sang số thập phân
 # rồi lưu lại vào file  2to10.out (mỗi dòng 1 số)
 #Đọc file và print liệu
+
+# CACH 2:
+# f = open("2to10.inp","r")
+# data = f.readlines()
+# f.close()
+# wf = open("2to10.out", "w")
+# for i in range(len(data)):
+#     data[i] = int("0b"+data[i],2)
+#     data[i]=str(data[i])
+#     print(data[i])
+#     wf.writelines(f"{data[i]}\n")
+
+
+# CACH 1:
 with open("2to10.inp", "r") as f:
     line = f.readline().rstrip("\n")
     index = 1
@@ -20,5 +34,6 @@ with open("2to10.inp", "r") as f:
             wf.write(f'{line} -> {Decimal}\n')
         index += 1
         line = f.readline().rstrip("\n")
+    f.close()
 with open('2to10.out', 'a') as wf:
     wf.write('\n')
